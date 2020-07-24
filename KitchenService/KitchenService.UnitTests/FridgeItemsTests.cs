e9afb46fa9cf1a7020d7a301e770ae7e6d2fa085
Nick Escalona <nicholas.escalona@gmail.com>
2020-07-24 11:33:43 -0500

    add some coverage

---
+++ KitchenService/KitchenService.UnitTests/FridgeItemsTests.cs
@@ -0,0 +1,17 @@
+﻿using KitchenService.Core;
+using Xunit;
+
+namespace KitchenService.UnitTests
+{
+    public class FridgeItemsTests
+    {
+        public void ConstructorConstructsWithDefaults()
+        {
+            var item = new FridgeItem();
+
+            Assert.Equal(expected: 0, actual: item.Id);
+            Assert.Null(item.Name);
+            Assert.Null(item.ExpirationDate);
+        }
+    }
+}
