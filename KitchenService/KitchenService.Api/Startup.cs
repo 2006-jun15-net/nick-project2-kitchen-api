18d3e89e67602daba8fbc4954fc5db36c13d1131
escalonn <nicholas.escalona@gmail.com>
2020-08-06 20:04:21 +0000

    .

--- KitchenService/KitchenService.Api/Startup.cs
+++ KitchenService/KitchenService.Api/Startup.cs
@@ -72,7 +72,7 @@ namespace KitchenService.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
